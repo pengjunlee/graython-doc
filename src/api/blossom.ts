72d677ec02a10f0f11a49cd95da1f26f35af36a9
pengjunlee <pengjunlee@163.com>
2024-06-25 22:07:46 +0800

    2

--- src/api/blossom.ts
+++ src/api/blossom.ts
@@ -12,35 +12,6 @@ export const docTreeOpenApi = (params?: object): Promise<R<any>> => {
 }
 
 /**
- * 专题列表
- * @param params
- * @returns
- */
-export const subjectsApi = (params?: object): Promise<R<any>> => {
-  return rq.get<R<any>>('/folder/subjects/open', { params })
-}
-
-// --------------------------------------------------< 文章 >--------------------------------------------------
-
-/**
- * 文章热力图
- * @param params
- * @returns
- */
-export const articleHeatmapApi = (params?: object): Promise<R<any>> => {
-  return rq.get<R<any>>('/article/stat/heatmap/open', { params })
-}
-
-/**
- * 字数折线图
- * @param params
- * @returns
- */
-export const articleWordLineApi = (params?: object): Promise<R<any>> => {
-  return rq.get<R<any>>('/article/stat/line/open', { params })
-}
-
-/**
  * 文章详情
  * @param params
  * @returns
