db759b8b82fd7d9fc267385498ca774d72bdaaa4
pengjunlee <pengjunlee@163.com>
2024-06-27 20:40:51 +0800

    2

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,18 @@ import BLRow from '@/components/BLRow.vue'
 import BLCol from '@/components/BLCol.vue'
 import BLTag from '@/components/BLTag.vue'
 
+window.blconfig = {
+    /**
+     * 基础配置
+     */
+    SYS: {
+      // 网站名称
+      NAME: 'Graython个人文档',
+      LOGO: '/favicon.png',
+      GITHUB_URL:'/graython-doc'
+    }
+  }
+
 const app = createApp(App)
 app.use(pinia)
 app.use(router)
